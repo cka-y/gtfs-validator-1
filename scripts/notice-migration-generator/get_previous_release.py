1f89187a1affe1e10cda1e3dd2bbd33b04bf3d79
cka-y <chaimaa@mobilitydata.org>
2023-07-07 17:12:33 -0400

    testing #1524 for release

--- scripts/notice-migration-generator/get_previous_release.py
+++ scripts/notice-migration-generator/get_previous_release.py
@@ -4,5 +4,9 @@ from utils.utils import get_migration_file
 if __name__ == '__main__':
     migration_table = get_migration_file()
     previous_version = sorted(list(migration_table.columns))[-1].lower()
-    os.environ["PREVIOUS_VERSION"] = previous_version
+
+    env_file = os.getenv('GITHUB_ENV')
+    with open(env_file, "a") as gh_env:
+        gh_env.write(f"PREVIOUS_VERSION={previous_version}\n")
+
     print(previous_version)
