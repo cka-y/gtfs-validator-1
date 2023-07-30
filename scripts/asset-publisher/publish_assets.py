769b3e487ebc0b7f5413d57db833a2b83e6e810c
cka-y <chaimaa@mobilitydata.org>
2023-07-29 21:02:13 -0400

    update

---
+++ scripts/asset-publisher/publish_assets.py
@@ -0,0 +1,36 @@
+import os
+import glob
+import requests
+
+os_name = os.getenv("OS_NAME").split("-")[0]
+github_token = os.getenv("GITHUB_TOKEN")
+release_id = os.getenv("RELEASE_ID")
+owner = os.getenv("OWNER")
+repo = os.getenv("REPO")
+
+print(os_name)
+print(github_token)
+print(release_id)
+print(owner)
+print(repo)
+
+# headers = {
+#     "Authorization": f"token {github_token}",
+#     "Accept": "application/vnd.github.v3+json",
+# }
+#
+# for file in glob.glob("./app/pkg/build/jpackage/*"):
+#     extension = file.split('.')[-1]
+#     if extension not in ["msi", "dmg", "pkg", "deb"]:
+#         continue
+#
+#     print(f'Uploading {file}')
+#
+#     with open(file, 'rb') as f:
+#         response = requests.post(
+#             f"https://uploads.github.com/repos/{owner}/{repo}/releases/{release_id}/assets?name=Installer%20{os_name}.{extension}",
+#             headers=headers,
+#             data=f.read()
+#         )
+#
+#     response.raise_for_status()
