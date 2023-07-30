4f41cea668113e253152d8bb4a0919a44db7843f
cka-y <chaimaa@mobilitydata.org>
2023-07-29 21:31:26 -0400

    complete

--- scripts/asset-publisher/publish_assets.py
+++ scripts/asset-publisher/publish_assets.py
@@ -8,29 +8,23 @@ release_id = os.getenv("RELEASE_ID")
 owner = os.getenv("OWNER")
 repo = os.getenv("REPO")
 
-print(os_name)
-print(github_token)
-print(release_id)
-print(owner)
-print(repo)
+headers = {
+    "Authorization": f"token {github_token}",
+    "Accept": "application/vnd.github.v3+json",
+}
 
-# headers = {
-#     "Authorization": f"token {github_token}",
-#     "Accept": "application/vnd.github.v3+json",
-# }
-#
-# for file in glob.glob("./app/pkg/build/jpackage/*"):
-#     extension = file.split('.')[-1]
-#     if extension not in ["msi", "dmg", "pkg", "deb"]:
-#         continue
-#
-#     print(f'Uploading {file}')
-#
-#     with open(file, 'rb') as f:
-#         response = requests.post(
-#             f"https://uploads.github.com/repos/{owner}/{repo}/releases/{release_id}/assets?name=Installer%20{os_name}.{extension}",
-#             headers=headers,
-#             data=f.read()
-#         )
-#
-#     response.raise_for_status()
+for file in glob.glob("./app/pkg/build/jpackage/*"):
+    extension = file.split('.')[-1]
+    if extension not in ["msi", "dmg", "pkg", "deb"]:
+        continue
+
+    print(f'Uploading {file}')
+
+    with open(file, 'rb') as f:
+        response = requests.post(
+            f"https://uploads.github.com/repos/{owner}/{repo}/releases/{release_id}/assets?name=Installer%20{os_name}.{extension}",
+            headers=headers,
+            data=f.read()
+        )
+
+    response.raise_for_status()
